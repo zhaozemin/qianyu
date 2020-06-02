a5f279839d8636f0823253997db96ef80dc6423e
vagrant <vagrant@homestead>
2020-06-02 02:16:23 +0000

    商品属性模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,21 +10,27 @@ class Product extends Model
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
     public static $typeMap = [
-        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_NORMAL => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
     ];
 
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price','type'
+        'rating', 'sold_count', 'review_count', 'price', 'type'
     ];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
     ];
+
     // 与商品SKU关联
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+
+    public function properties()
+    {
+        return $this->hasMany(ProductProperty::class);
     }
 
     public function getImageUrlAttribute()
@@ -41,7 +47,19 @@ class Product extends Model
         return $this->belongsTo(Category::class);
     }
 
-    public function crowdfunding(){
+    public function crowdfunding()
+    {
         return $this->hasOne(CrowdfundingProduct::class);
+    }
+
+    public function getGroupedPropertiesAttribute()
+    {
+        return $this->properties
+            // 按照属性名聚合，返回的集合的 key 是属性名，value 是包含该属性名的所有属性集合
+            ->groupBy('name')
+            ->map(function ($properties) {
+                // 使用 map 方法将属性集合变为属性值集合
+                return $properties->pluck('value')->all();
+            });
     }
 }
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -110,7 +110,19 @@
                         </ul>
                         <div class="tab-content">
                             <div role="tabpanel" class="tab-pane active" id="product-detail-tab">
-                                {!! $product->description !!}
+                                <!-- 产品属性开始 -->
+                                <div class="properties-list">
+                                    <div class="properties-list-title">产品参数：</div>
+                                    <ul class="properties-list-body">
+                                        @foreach($product->grouped_properties as $name => $value)
+                                            <li>{{ $name }}：{{ join(' ',$value) }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                                <!-- 产品属性结束 -->
+                                <div class="product-description">
+                                    {!! $product->description !!}
+                                </div>
                             </div>
                             <div role="tabpanel" class="tab-pane" id="product-reviews-tab">
                                 <!-- 评论列表开始 -->
