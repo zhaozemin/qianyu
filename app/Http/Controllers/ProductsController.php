bb355b1acaab34bda81e335d9f3df2b6f3516530
vagrant <vagrant@homestead>
2020-06-02 13:36:40 +0000

    分面搜索

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -15,13 +15,13 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $page    = $request->input('page', 1);
+        $page = $request->input('page', 1);
         $perPage = 16;
 
         $params = [
-            'index' =>'products',
-            'body' =>[
-                'from' => ($page - 1) *$perPage,
+            'index' => 'products',
+            'body' => [
+                'from' => ($page - 1) * $perPage,
                 'size' => $perPage,
                 'query' => [
                     'bool' => [
@@ -35,11 +35,11 @@ class ProductsController extends Controller
         // 是否有提交 order 参数，如果有就赋值给 $order 变量
         // order 参数用来控制商品的排序规则
 
-        if ($order = $request->input('order','')) {
+        if ($order = $request->input('order', '')) {
             // 是否是以 _asc 或者 _desc 结尾
-            if (preg_match('/^(.+)_(asc|desc)$/',$order,$m)){
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
                 // 如果字符串开头是着 3个 字符串之一，说明是一个合法的排序值
-                if (in_array($m[1],['price','sold_count','rating'])){
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
                     // 根据传入的排序值来构造排序参数
                     $params['body']['sort'] = [[$m[1] => $m[2]]];
                 }
@@ -50,7 +50,7 @@ class ProductsController extends Controller
             if ($category->is_directory) {
                 // 如果是一个父类目，则使用 category_path 来筛选
                 $params['body']['query']['bool']['filter'][] = [
-                    'prefix' => ['category_path' => $category->path.$category->id.'-'],
+                    'prefix' => ['category_path' => $category->path . $category->id . '-'],
                 ];
             } else {
                 // 否则直接通过 category_id 筛选
@@ -67,7 +67,7 @@ class ProductsController extends Controller
                 $params['body']['query']['bool']['must'][] = [
                     [
                         'multi_match' => [
-                            'query'  => $keyword,
+                            'query' => $keyword,
                             'fields' => [
                                 'title^3',
                                 'long_title^2',
@@ -83,8 +83,76 @@ class ProductsController extends Controller
             }
 
         }
+        // 只有当用户有输入搜索词或者使用了类目筛选的时候才会做聚合
+        if ($search || isset($category)) {
+            $params['body']['aggs'] = [
+                'properties' => [
+                    'nested' => [
+                        'path' => 'properties',
+                    ],
+                    'aggs' => [
+                        'properties' => [
+                            'terms' => [
+                                'field' => 'properties.name',
+                            ],
+                            'aggs' => [
+                                'value' => [
+                                    'terms' => [
+                                        'field' => 'properties.value',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ];
+        }
+        // 定义一个数组
+        $propertyFilters = [];
+        // 从用户请求参数获取 filters
+        if ($filterString = $request->input('filters')) {
+            // 将获取到的字符串用符号 | 拆分成数组
+            $filterArray = explode('|', $filterString);
+            foreach ($filterArray as $filter) {
+                // 将字符串用符号 : 拆分成两部分并且分别赋值给 $name 和 $value 两个变量
+                list($name, $value) = explode(':', $filter);
+                // 将用户筛选的属性添加到数组中
+                $propertyFilters[$name] = $value;
+                // 添加到 filter 类型中
+                $params['body']['query']['bool']['filter'][] = [
+                    // 由于我们要筛选的是 nested 类型下的属性，因此需要用 nested 查询
+                    'nested' => [
+                        // 指明 nested 字段
+                        'path' => 'properties',
+                        'query' => [
+                            ['term' => ['properties.name' => $name]],
+                            ['term' => ['properties.value' => $value]],
+                        ],
+                    ],
+                ];
+            }
+        }
+
 
         $result = app('es')->search($params);
+
+        $properties = [];
+        // 如果返回结果里有 aggregations 字段，说明做了分面搜索
+        if (isset($result['aggregations'])) {
+            // 使用 collect 函数将返回值转为集合
+            $properties = collect($result['aggregations']['properties']['properties']['buckets'])
+                ->map(function ($bucket) {
+                    // 通过 map 方法取出我们需要的字段
+                    return [
+                        'key' => $bucket['key'],
+                        'values' => collect($bucket['value']['buckets'])->pluck('key')->all(),
+                    ];
+                })
+                ->filter(function ($property) use ($propertyFilters) {
+                    // 过滤掉只剩下一个值 或者 已经在筛选条件里的属性
+                    return count($property['values']) > 1 && !isset($propertyFilters[$property['key']]) ;
+                });
+        }
 
         // 通过 collect 函数将返回结果转为集合，并通过集合的 pluck 方法取到返回的商品 ID 数组
         $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
@@ -102,15 +170,16 @@ class ProductsController extends Controller
 
         return view('products.index', [
             'products' => $pager,
-            'filters'  => [
+            'filters' => [
                 'search' => $search,
-                'order'  => $order,
+                'order' => $order,
             ],
-            // 等价于 isset($category) ? $category : null
-            'category' =>  $category ?? null,
-            // 将类目树传递给模板文件
+            'category' => $category ?? null,
+            'properties' => $properties,
+            'propertyFilters' => $propertyFilters,
         ]);
     }
+
     public function show(Product $product, Request $request)
     {
         // 判断商品是否已经上架，如果没有上架则抛出异常。
@@ -120,15 +189,15 @@ class ProductsController extends Controller
 
         $favored = false;
         // 用户未登录时返回的是 null，已登录时返回的是对应的用户对象
-        if($user = $request->user()) {
+        if ($user = $request->user()) {
             // 从当前用户已收藏的商品中搜索 id 为当前商品 id 的商品
             // boolval() 函数用于把值转为布尔值
             $favored = boolval($user->favoriteProducts()->find($product->id));
         }
 
         $reviews = OrderItem::query()
-            ->with(['productSku','order.user'])
-            ->where('product_id',$product->id)
+            ->with(['productSku', 'order.user'])
+            ->where('product_id', $product->id)
             ->whereNotNull('reviewed_at')
             ->limit(10)
             ->get();
