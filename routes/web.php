f62c2dc40c45adcd88ad245f04852792b1e4b8de
vagrant <vagrant@homestead>
2020-06-23 16:06:33 +0000

    随机拒绝

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store');
+Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store')->middleware('random_drop:50');
 // 在之前的路由后面配上中间件
 //Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 //Route::get('/', 'PagesController@root')->name('root');
